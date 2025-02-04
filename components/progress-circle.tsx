398631d028607a7e7759fda8c7cd0b0134466931
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-02-04 17:21:30 +0400

    modified progress circle styles

--- components/progress-circle.tsx
+++ components/progress-circle.tsx
@@ -1,38 +1,37 @@
 export default function ProgressCircle({ percentage = 40 }) {
-    const circumference = 2 * Math.PI * 45 // radius is 45
-    const strokeDashoffset = circumference - (percentage / 100) * circumference
+  const radius = 90; // Reduced by 10%
+  const circumference = 2 * Math.PI * radius;
+  const strokeDashoffset = circumference - (percentage / 100) * circumference;
   
-    return (
-      <div className="relative inline-flex items-center justify-center">
-        <svg className="transform -rotate-90 w-24 h-24">
-          <circle
-            className="text-zinc-800"
-            strokeWidth="4"
-            stroke="currentColor"
-            fill="transparent"
-            r="45"
-            cx="48"
-            cy="48"
-          />
-          <circle
-            className="text-emerald-500"
-            strokeWidth="4"
-            strokeDasharray={circumference}
-            strokeDashoffset={strokeDashoffset}
-            strokeLinecap="round"
-            stroke="currentColor"
-            fill="transparent"
-            r="45"
-            cx="48"
-            cy="48"
-          />
-        </svg>
-        <div className="absolute flex flex-col items-center justify-center text-center">
-          <span className="text-2xl font-medium text-emerald-500">{percentage}%</span>
-          <span className="text-xs text-zinc-400">complete</span>
-        </div>
-      </div>
-    )
+  return (
+    <div className="relative bg-[#27272a] rounded-full inline-flex items-center justify-center">
+    <svg viewBox="-10 -10 200 200" className="transform -rotate-90 w-44 h-44">
+      <circle
+      className="text-zinc-800"
+      strokeWidth="12"
+      stroke="currentColor"
+      fill="transparent"
+      r={radius}
+      cx="90"
+      cy="90"
+      />
+      <circle
+      className="text-[#2563eb]"
+      strokeWidth="12"
+      strokeDasharray={circumference}
+      strokeDashoffset={strokeDashoffset}
+      strokeLinecap="butt"
+      stroke="currentColor"
+      fill="transparent"
+      r={radius}
+      cx="90"
+      cy="90"
+      />
+    </svg>
+    <div className="absolute flex flex-col items-center justify-center text-center">
+      <span className="text-4xl font-extralight text-[#2563eb]">{percentage}%</span>
+      <span className="text-base font-extralight text-zinc-400">complete</span>
+    </div>
+    </div>
+  )
   }
-  
-  
