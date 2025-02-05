f26fd7202555ca7264fae2be58e0cc7fd8ca12af
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-02-05 14:31:19 +0400

    moved home data arrays to seperate file

--- app/case/home.tsx
+++ app/case/home.tsx
@@ -3,6 +3,7 @@ import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carouse
 import { ArrowLeft, ArrowRight } from "lucide-react"
 import { useEffect, useState } from "react"
 import Cookies from "js-cookie"
+import { cardData, furtherSteps } from "./homeData"
 
 function ProgressBar({ percentage = 40 }) {
   return (
@@ -70,66 +71,6 @@ useEffect(() => {
     setUserName(name)
   }
 }, [])
-
-  const furtherSteps = [
-    {
-      title: "Confirm visa Appointment",
-      description: "Confirm your secured residency permit",
-    },
-    {
-      title: "Confirm School Setup",
-      description: "Set up enrollment details for your children's schooling in Abu Dhabi",
-    },
-    {
-      title: "Locate Hospitals Nearby",
-      description: "Find quality healthcare providers and facilities near you",
-    },
-    {
-      title: "Apply for a Driver's License",
-      description: "Start the process to get your official UAE driving license",
-    },
-    {
-      title: "Obtain Medical Insurance",
-      description: "Ensure you have adequate health coverage in the UAE",
-    },
-    {
-      title: "Register for a Mobile SIM Card",
-      description: "Stay connected by choosing the best telecom provider in the UAE",
-    },
-    {
-      title: "Set up a Bank Account",
-      description: "Open a UAE bank account for seamless financial transactions",
-    },
-    {
-      title: "Connect with Expat Communities",
-      description: "Join local groups to share experiences and receive support",
-    },
-    {
-      title: "Learn about Emirati Culture",
-      description: "Discover local customs and traditions specific to Abu Dhabi",
-    },
-    {
-      title: "Explore UAE Infrastructure",
-      description: "Get informed about public transport, utilities, and connectivity services",
-    },
-  ]
-
-  const cardData = [
-    {
-      title: "Get an Emirates ID",
-      content:
-        "An Emirates ID grants residents of the UAE a proof of identification and is required for most things in dubai ",
-    },
-    {
-      title: "Open a new bank account",
-      content:
-        "You will need a UAE bank account for essential daily transactions, Some banks offer free international transfers for the first few months!",
-    },
-    {
-      title: "Set up a mobile SIM card",
-      content: "A UAE SIM card is essential for local calls, mobile payments, and OTP verifications",
-    },
-  ]
 
   if (overviewMode) {
     return (
