6dd9f730811ab2662e12046add95138133a2d571
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-02-11 17:16:14 +0400

    switched background to a webp

--- variables/backgrouds.ts
+++ variables/backgrouds.ts
@@ -1,4 +1,4 @@
 export const backgrounds = [
-    "https://images.pexels.com/photos/1473673/pexels-photo-1473673.jpeg",
+    "/images/background.webp",
   //  "https://images.pexels.com/photos/442579/pexels-photo-442579.jpeg",
   ];
