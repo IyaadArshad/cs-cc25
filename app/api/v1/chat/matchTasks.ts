b61d9b1159e5a31154f2c9729488798f66d9d8b7
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-02-17 12:17:25 +0400

    refactor matchTasks.ts

--- app/api/v1/chat/matchTasks.ts
+++ app/api/v1/chat/matchTasks.ts
@@ -1,32 +1,33 @@
 export const matchTasks = {
-    "visa": {
-        "visa-work": "Work Visa",
-        "visa-tourist": "Tourist Visa",
-        "visa-other": "Not specified"
-    },
-    "school": {
-        "school-confirmed": "The user has their children's/child's schooling setup",
-        "school-nochild": "The user does not have children",
-        "school-inprogress": "The user is in the process of selecting a school"
-    },
-    "dlicense": {
-        "dlicense-confirmed": "The user has a driving license",
-        "dlicense-confimed": "The user has a driving license", // alias added for typo correction
-        "dlicense-none": "The user does not have a driving license",
-        "dlicense-inprogress": "The user is in the process of getting a driving license"
-    },
-    "insurance": {
-        "insurance-confirmed": "The user has medical insurance",
-        "insurance-none": "The user has no medical insurance"
-    },
-    "sim": {
-        "sim-confirmed": "The user has mobile coverage",
-        "sim-none": "The user has no mobile coverage",
-        "sim-wouldlike": "The user has no coverage, but would like one"
-    },
-    "bank": {
-        "bank-confirmed": "The user has a bank account",
-        "bank-mid": "The user is in the process of opening a bank account",
-        "bank-none": "The user has no bank account"
-    }
-}
+  visa: {
+    "visa-work": "Work Visa",
+    "visa-tourist": "Tourist Visa",
+    "visa-other": "Not specified",
+  },
+  school: {
+    "school-confirmed": "The user has their children's/child's schooling setup",
+    "school-nochild": "The user does not have children",
+    "school-inprogress": "The user is in the process of selecting a school",
+  },
+  dlicense: {
+    "dlicense-confirmed": "The user has a driving license",
+    "dlicense-confimed": "The user has a driving license", // alias added for typo correction
+    "dlicense-none": "The user does not have a driving license",
+    "dlicense-inprogress":
+      "The user is in the process of getting a driving license",
+  },
+  insurance: {
+    "insurance-confirmed": "The user has medical insurance",
+    "insurance-none": "The user has no medical insurance",
+  },
+  sim: {
+    "sim-confirmed": "The user has mobile coverage",
+    "sim-none": "The user has no mobile coverage",
+    "sim-wouldlike": "The user has no coverage, but would like one",
+  },
+  bank: {
+    "bank-confirmed": "The user has a bank account",
+    "bank-mid": "The user is in the process of opening a bank account",
+    "bank-none": "The user has no bank account",
+  },
+};
