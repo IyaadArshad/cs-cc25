6affff2c9a89ae41bab2ee471e678e8ac58bec7c
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-02-18 18:51:35 +0400

    refactor mode-transition.tsx

--- components/mode-transition.tsx
+++ components/mode-transition.tsx
@@ -1,12 +1,9 @@
-import { Loader2 } from "lucide-react";
-
 interface ModeTransitionProps {
   mode: "expanding" | "minimizing";
 }
 
 export default function ModeTransition({ mode }: ModeTransitionProps) {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center gap-6">
-    </div>
+    <div className="flex-1 flex flex-col items-center justify-center gap-6"></div>
   );
 }
