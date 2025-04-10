dd8d3143962963f066601f405ac13262e20c8366
ElliasJones <hello@iyaadarshad.com>
2025-04-10 13:24:27 +0400

    Modified articles listing and content

--- app/api/v1/feed/content.ts
+++ app/api/v1/feed/content.ts
@@ -32,17 +32,6 @@ export const news = [
       "The growing India-UAE partnership, particularly through the key economic hubs of Mumbai and Dubai, is reshaping the business landscape and presenting immense opportunities across diverse sectors, top officials said. The Dubai-India Business Forum, held in Mumbai on Tuesday, gathered 200 industry leaders.",
   },
   {
-    title:
-      "India-UAE travel: Dubai-India aviation ties soar, as Emirates carries 90m passengers over 40 years",
-    link: "https://gulfnews.com/uae/transport/india-uae-travel-dubai-india-aviation-ties-soar-as-emirates-carries-90m-passengers-over-40-years-1.102394713",
-    source: "Gulf News",
-    date: "April 10, 2025",
-    author: "Sajila Saseendran",
-    position: "Chief Reporter",
-    summary:
-      "Emirates Airline, a key symbol of the UAE’s robust ties with India, has carried more than 90 million passengers between Dubai and India since it began operations in 1985. This was revealed amid the first official India visit by Sheikh Hamdan bin Mohammed bin Rashid Al Maktoum.",
-  },
-  {
     title: "Dubai banks hold 46% of UAE deposits, exceeding Dh1.3 trillion",
     link: "https://gulfnews.com/business/banking-insurance/dubai-banks-hold-46-of-uae-deposits-exceeding-dh13-trillion-1.102394856",
     source: "Gulf News",
@@ -72,5 +61,69 @@ export const news = [
     position: "Your Money Editor",
     summary:
       "Dubai’s stock index tumbled on Thursday, poised for its biggest daily decline this year, and other Gulf bourses slid after US president Donald Trump announced wide-ranging tariffs on importers. Losers outnumbered gainers 34 to 10 on Dubai’s benchmark, which was down as 1.8 per cent, mainly due to Emaar Properties' steep 8.9 per cent drop, while Abu Dhabi’s index slipped 0.8 per cent, signaling a regional downturn.",
+  },
+  {
+    title:
+      "​​eTA: Visa-free travel for 13 countries to Canada, what you need to know",
+    link: "https://gulfnews.com/world/americas/eta-visa-free-travel-for-13-countries-to-canada-what-you-need-to-know-1.102394673",
+    source: "Gulf News",
+    date: "April 10, 2025",
+    author: "Jay Hilotin",
+    position: "Senior Assistant Editor",
+    summary:
+      "In 2023, the Canadian government announced the expansion of the electronic travel authorisation (eTA) programme to include eligible travellers from 13 additional countries. It means more people are welcome to come to Canada without visa under changes to the federal programme.",
+  },
+  {
+    title:
+      "Resilient by design: How NCEMA shapes emergency preparedness in the UAE",
+    link: "https://gulfnews.com/opinion/op-eds/resilient-by-design-how-ncema-shapes-emergency-preparedness-in-the-uae-1.102394692",
+    source: "Gulf News",
+    date: "April 10, 2025",
+    author: "Kristian P. Alexander",
+    position: "Special to Gulf News",
+    summary:
+      "The National Emergency Crisis and Disaster Management Authority (NCEMA) stands as a cornerstone of the UAE government’s commitment to safeguarding its citizens, residents, and critical assets. Far from being merely a reactive institution, NCEMA is a proactive, strategic agency whose relevance extends across all levels of governance — playing a central role in strengthening national resilience in an increasingly volatile world.",
+  },
+  {
+    title: "Trump, tariffs and a changing world order",
+    link: "https://gulfnews.com/opinion/op-eds/on-point-trump-tariffs-and-a-changing-world-order-1.102394399",
+    source: "Gulf News",
+    date: "April 09, 2025",
+    author: "Jyotsna Mohan",
+    position: "Special to Gulf News",
+    summary:
+      "‘Give me your tired, your poor, your huddled masses yearning to breathe free’ - the iconic words inscribed on the Statue of Liberty have been an abiding promise, a symbol of the great American Dream. In Trump’s America, however, this ethos is being stripped, and unfortunately, what happens in the country does not stay in the country. At the time of writing this, I can only say, fasten your seatbelts!",
+  },
+  {
+    title: "Sharjah launches fire safety campaign for industrial areas",
+    link: "https://gulfnews.com/uae/emergencies/sharjah-launches-fire-safety-campaign-for-industrial-areas-1.102394762",
+    source: "Gulf News",
+    date: "April 10, 2025",
+    author: "Aghaddir Ali",
+    position: "Senior Reporter",
+    summary:
+      "Dubai: The Sharjah City Municipality and Sharjah Civil Defence Authority on Wednesday announced to launch an important safety initiative in a unified effort to enhance fire safety standards across industrial areas. Titled “Industrial Facility Safety is Everyone’s Responsibility,” the campaign seeks to boost awareness among industrial facility owners regarding fire prevention and safety measures, with support from the Sharjah Economic Development Department.",
+  },
+  {
+    title:
+      "Dubai Chamber’s new office in India ‘strategic step’ to boost economic cooperation: Sheikh Hamdan",
+    link: "https://gulfnews.com/business/economy/dubai-chambers-new-office-in-india-strategic-step-to-boost-economic-cooperation-sheikh-hamdan-1.102394791",
+    source: "Gulf News",
+    date: "April 10, 2025",
+    author: "Ashwani Kumar",
+    position: "Chief Reporter",
+    summary:
+      "Mumbai: In a move to further deepen Dubai’s economic relations with India, the Dubai International Chamber has opened its second representative office in the country. Sheikh Hamdan bin Mohammed bin Rashid Al Maktoum, Crown Prince of Dubai, Deputy Prime Minister, Minister of Defence, and Chairman of the Executive Council of Dubai, who attended the launch of the new office in Bengaluru, said the initiative represents a strategic step that will elevate economic cooperation between Dubai and India.",
+  },
+  {
+    title:
+      "Driving in the UAE? These six updated speed limits could save you from fines",
+    link: "https://gulfnews.com/living-in-uae/transport/driving-in-the-uae-these-six-updated-speed-limits-could-save-you-from-fines-1.102394869",
+    source: "Gulf News",
+    date: "April 10, 2025",
+    author: "Zainab Husain",
+    position: "Features Writer",
+    summary:
+      "Dubai: Speeding remains one of the most dangerous driving behaviours on UAE roads, with over 10 million violations recorded in 2024 alone, according to a recent statistic released by the Ministry of Interior (MOI). Authorities have repeatedly warned that excessive speed is a leading cause of deadly accidents, such as vehicle rollovers, run-overs, rear-end collisions, and vehicles veering off the road.",
   },
 ];
