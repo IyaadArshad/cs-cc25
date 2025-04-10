3c6c394aec3e9447edc9d65dc7bf039b6fd91b2b
ElliasJones <hello@iyaadarshad.com>
2025-04-10 17:34:32 +0400

    add order by date

--- app/api/v1/feed/route.ts
+++ app/api/v1/feed/route.ts
@@ -1,7 +1,12 @@
+import { news } from './content';
+
 export async function GET() {
     try {
-        const response = await fetch(
-            "https://cdn.query.prod.cms.msn.com/cms/api/amp/search?%20$top=100&$filter=%27$type%27eq%27article%27and%27_locale%27eq%27en-ae%27"
+//        const response = await fetch(
+  //          "https://cdn.query.prod.cms.msn.com/cms/api/amp/search?%20$top=1000&$filter=%27$type%27eq%27article%27and%27_locale%27eq%27en-ae%27"
+    //    );
+            const response = await fetch(
+          "https://cdn.query.prod.cms.msn.com/cms/api/amp/search?%20$top=200"
         );
         
         if (!response.ok) {
@@ -13,7 +18,19 @@ export async function GET() {
             });
         }
         
-        const data = await response.json();
+        let data = await response.json();
+
+        // Concatenate the news array with the data from the API
+        if (data && Array.isArray(data)) {
+            data = news.concat(data);
+            
+            // Sort the data array by _lastPublishedDateTime in descending order
+            data.sort((a, b) => {
+                const dateA = new Date(a._lastPublishedDateTime || a.date);
+                const dateB = new Date(b._lastPublishedDateTime || b.date);
+                return dateB.getTime() - dateA.getTime(); // Sort in descending order
+            });
+        }
         
         return new Response(JSON.stringify(data), {
             status: 200,
