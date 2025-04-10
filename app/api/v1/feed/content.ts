0406432eb36793a424efb9e49bdd0812218db464
ElliasJones <hello@iyaadarshad.com>
2025-04-10 13:00:22 +0400

    add content

---
+++ app/api/v1/feed/content.ts
@@ -0,0 +1,11 @@
+export const news = [
+    {
+        "title": "Dubai gold rate surges as Trump tariff relief takes over markets",
+        "link": "https://gulfnews.com/business/retail/dubai-gold-rate-surges-as-trump-tariff-relief-takes-over-markets-1.500088948",
+        "source": "Gulf News",
+        "date": "April 10, 2025",
+        "author": "Manoj Nair",
+        "position": "Business Editor",
+        "summary": "UAE gold price has increased by nearly Dh13 a gram for 22K gold in under two days, reaching Dh347 per gram after the US announced a pause on tariffs for all trading partners except China. Gold prices reached $3,116 an ounce, approaching the record Dubai gold rate of Dh351.75 set on April 3. This sharp increase is concerning for shoppers preparing for the April 30 'Akshaya Trithiya' gold buying event, with jewelers needing to rework their promotions to ensure customer turnout."
+    }
+]
