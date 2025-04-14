d871317f9e79c7783e5ff4dec7e025521bec336c
ElliasJones <hello@iyaadarshad.com>
2025-04-14 19:24:11 +0400

    fix translucent background issues

--- app/case/guide.tsx
+++ app/case/guide.tsx
@@ -517,7 +517,7 @@ export default function CaseGuide() {
   );
 
   return (
-    <div ref={containerRef} className="flex-1 p-5 overflow-y-auto bg-gray-950">
+    <div ref={containerRef} className="flex-1 p-5 overflow-y-auto">
       <AnimatePresence mode="wait">
         {selectedCard ? (
           <motion.div
